e25a1177b0d5d726c0d7b2cad4aac41428d62390
juan galarce <j6alarce@gmail.com>
2020-12-04 12:46:18 -0300

    first commit

--- models/server.js
+++ models/server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const http = require("http");
 const socketio = require("socket.io");
 const path = require("path");
+const cors = require('cors');
+
 const Sockets = require("./sockets");
 
 class Server {
@@ -22,6 +24,9 @@ class Server {
   middlewares() {
     // Desplegar el directorio público
     this.app.use(express.static(path.resolve(__dirname, "../public")));
+
+    // CORS
+    this.app.use( cors() );
   }
 
   configurationSockets() {
